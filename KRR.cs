c1731de1c1cb4b7611342df159914781655699a1
AL-JaaFari <raed.sam119@gmail.com>
2017-05-17 00:20:59 +0300

    Add files via upload

---
+++ KRR.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Agent
+    {
+        public string Name;
+
+        public Agent(string name)
+        {
+            this.Name = name;
+
+        }
+
+       
+
+
+        public void PerformAction()
+        {
+            
+        }
+
+    }
+
+
+
+
+
+
+    public class Fluent
+    {
+        public string Name;
+
+        public Fluent(string name)
+        {
+            this.Name = name;
+
+        }
+
+
+
+        public bool IsTrue()
+        {
+            if ()
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+
+        public class Action
+        {
+            public string Name;
+        public Action() { }
+
+
+        public void AddAction()
+        {
+            var list = new List<String>() { "Move", "Hit" };
+        }
+
+    }
+
+    
+
+        }
